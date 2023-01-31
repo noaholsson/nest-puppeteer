5e6b46fec17a069dcd6712b437655b3b99ce8d6a
Noah Olsson <noaholsson@hotmail.com>
2023-01-31 13:20:18 +0100

    Small fixes to make publish work

--- src/puppeteer.constants.ts
+++ src/puppeteer.constants.ts
@@ -1,11 +1,13 @@
-import type { LaunchOptions } from 'puppeteer';
+import type { BrowserConnectOptions, BrowserLaunchArgumentOptions, LaunchOptions } from 'puppeteer';
 
 export const PUPPETEER_INSTANCE_NAME = 'PuppeteerInstanceName';
 export const PUPPETEER_MODULE_OPTIONS = 'PuppeteerModuleOptions';
 
 export const DEFAULT_PUPPETEER_INSTANCE_NAME = 'DefaultPuppeteer';
 
-const args: LaunchOptions['args'] = [
+type NestLaunchOptions = LaunchOptions & BrowserLaunchArgumentOptions & BrowserConnectOptions
+
+const args: NestLaunchOptions['args'] = [
   '--allow-insecure-localhost', // Enables TLS/SSL errors on localhost to be ignored (no interstitial, no blocking of requests).
   '--allow-http-screen-capture', // Allow non-secure origins to use the screen capture API and the desktopCapture extension API.
   '--no-zygote', // https://codereview.chromium.org/2384163002
@@ -15,7 +17,7 @@ if (typeof process.getuid === 'function') {
   args.push('--no-sandbox');
 }
 
-export const DEFAULT_CHROME_LAUNCH_OPTIONS: LaunchOptions = {
+export const DEFAULT_CHROME_LAUNCH_OPTIONS: NestLaunchOptions = {
   headless: true,
   pipe: process.platform !== 'win32',
   args,
